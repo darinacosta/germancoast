7ce5eb497cede257776ddbe52390b56e9c4fd90e
darinacosta <darinjacosta@gmail.com>
2014-11-24 06:31:50 -0600

    norco land use

--- layers/norco_landuse_dissolve_v1.js
+++ layers/norco_landuse_dissolve_v1.js
@@ -1,4 +1,4 @@
-var exp_norcolandusescompleteDISSOLVEv1 = {
+var norco_landuse_dissolve_v1 = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
