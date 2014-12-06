fba14addca286e1192ba1b337dc730d4ba4fe3d3
darinacosta <darinjacosta@gmail.com>
2014-12-06 13:23:30 -0600

    polyclick

--- js/app/labranche.js
+++ js/app/labranche.js
@@ -1,45 +1,35 @@
-define(['app/layers'],
+define(['app/layers',
+        'app/layerControl'],
 
-  function(layers){
+  function(layers,layerControl){
 
-    var executeDevelopmentEvent = function(selector, id, context){
+    var labrancheDevelopments = layers.labrancheDevelopments,
+    
+    executeDevelopmentEvent = function(selector, id, context){
       $('.geometry-link').css('text-decoration','none');
       $(selector).css('text-decoration','underline');
       $('#labranche-context').html(context);
     },
 
-    clickButton = function() {
-      $.each(layers.labrancheDevelopments._layers, function(key, value){
+    clickPoly = function() {
+      $.each(labrancheDevelopments._layers, function(key, value){
         if (value.feature.properties.NAME === "LaBranche Industrial Park") {
-          value.fire('click',{latlng:[30.0405664305846,-90.3017807006836]});
-          }
-        });
-      },
+          value.fireEvent('click',{latlng:[30.03759433988124,-90.37714004516602]});
+          map.setView(new L.LatLng(30.03759433988124,-90.37714004516602),14);
+        }
+      });
+    },
 
     activateGeometryLinks = $('#labranchePane').on("click", "a[href^='#']", function(event){
       if ($(event.target).hasClass('labranche-industrial-park')){
-        clickButton();
+        clickPoly();
         console.log('Labranche Industrial Park');
       }
     }),
     
     init =  function(){
-
-      console.log(layers.labrancheDevelopments);
-      
       activateGeometryLinks;
-
-      layers.labrancheDevelopments.on("click", function(e){
-        layers.labrancheDevelopments.setStyle({color: "#960000",
-            fillColor: "#642800",
-            fillOpacity: 0.4,
-            weight: 1})
-        e.layer.setStyle({
-          'fillColor':'rgb(130,150,0)',
-          'color':'rgb(200,200,0)'
-        });
-        map.setView(new L.LatLng(e.latlng['lat'],e.latlng['lng']),14);
-      })
+      layerControl.selectPolyOnClick(labrancheDevelopments, 'rgb(200,200,0)', 'rgb(130,150,0)');
     };
 
   return {init: init};
--- js/app/layerControl.js
+++ js/app/layerControl.js
@@ -2,9 +2,36 @@ define(['app/layers'],
 
   function(layers)
   {
+
     var hideAllLayers = function()
     {
       $.each(layers, function(key, val) {map.removeLayer(layers[key])});
-    }
-    return {hideAllLayers: hideAllLayers};
+    },
+
+    selectPolyOnClick = function(targetLayer, selectedColor, selectedFill){
+	    
+
+	    targetLayer.on("click", function(e){
+        //console.log(e);
+        console.log(targetLayer.style);
+        var targetPoly = e.layer,
+            originalFill = targetLayer.options.fillColor,
+            originalColor = targetLayer.options.color;
+
+	      targetLayer.setStyle({
+      	  color: originalColor,
+          fillColor: originalFill});
+
+	      targetPoly.setStyle({
+	        'fillColor': selectedFill,
+	        'color': selectedColor
+	      });
+
+	      map.setView(new L.LatLng(e.latlng['lat'],e.latlng['lng']),14);
+	    
+	    })
+	  };
+
+    return {hideAllLayers: hideAllLayers,
+            selectPolyOnClick: selectPolyOnClick};
   });
