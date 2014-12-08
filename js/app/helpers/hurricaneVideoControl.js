2cba58548182fe5587fd0c482009c3d96e3d8e30
unknown <djacosta@ITI-1109L.cityofno.com>
2014-12-08 14:41:15 -0600

    hurricaneVideoControl

--- js/app/helpers/hurricaneVideoControl.js
+++ js/app/helpers/hurricaneVideoControl.js
@@ -13,19 +13,19 @@ define(['app/vignettes/hurricane',
     hurricaneControls = function(){
       $('a.newpage').on("click", function(){
         $playButton.html('<span class="glyphicon glyphicon-play"></span>');
-       $hurricaneVideo.get(0).pause();
-       hurricaneVideoStatus='paused';
+        $hurricaneVideo.get(0).pause();
+        hurricaneVideoStatus='paused';
       });
 
       $('#hurricane-video, #hurricane-play').on("click", function(){
         if (hurricaneVideoStatus=='paused'){
-         $hurricaneVideo.get(0).play();
-         hurricaneVideoStatus='playing';
+          $hurricaneVideo.get(0).play();
+          hurricaneVideoStatus='playing';
           $playButton.html('<span class="glyphicon glyphicon-pause"></span>');
         }else if (hurricaneVideoStatus=='playing'){
           $playButton.html('<span class="glyphicon glyphicon-play"></span>');
-         $hurricaneVideo.get(0).pause();
-         hurricaneVideoStatus='paused';
+          $hurricaneVideo.get(0).pause();
+          hurricaneVideoStatus='paused';
         }
       });
 
@@ -36,7 +36,7 @@ define(['app/vignettes/hurricane',
         hurricane.init();
         layerControl.hideAllLayers();
         $hurricaneVideo.get(0).play();
-        //map.centerAndZoom([-90.4108, 30.0039], 13);
+        map.setView(new L.LatLng(30.0039, -90.4108), 13);
         if (hurricaneVideoStatus=='paused'){
           $playButton.html('<span class="glyphicon glyphicon-play"></span>');
         }else if (hurricaneVideoStatus=='playing'){
