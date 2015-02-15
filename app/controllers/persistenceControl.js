cc094d146ecb26a6754d73648f2558b8526a8552
darinacosta <darinjacosta@gmail.com>
2015-02-15 14:23:53 -0600

    static

--- app/controllers/persistenceControl.js
+++ app/controllers/persistenceControl.js
@@ -3,17 +3,19 @@ define(['jquery',
 
   function($){
 
+  serveStaticContent = (function(){
+    queryString = window.location.search.substring(1);
+    if (queryString.indexOf('_escaped_fragment_') > -1){
+      console.log('Serve static content');
+    }
+  })();
+
   init = function(){
     var hash = window.location.hash;
     $('.main-menu a[href="' + hash + '"]').trigger('click');
 
     $(document).on('click', '.main-menu', function (e) {
-      console.log(e);
-      if (e.target.className == 'home-location-button newpage'){
-        window.location.hash = '';
-      }else{
-        window.location.hash = e.target.hash;
-      }
+      window.location.hash = '!/' + e.target.hash.split('#')[1];
     });
   }
 
