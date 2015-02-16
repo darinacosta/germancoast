532c3436efb9374e99897862de90590a893604c2
darinacosta <darinjacosta@gmail.com>
2015-02-15 22:23:05 -0600

    static

---
+++ app/static/speculation_files/config.js
@@ -0,0 +1,32 @@
+var require = {
+    baseUrl: '.',
+    shim : {
+        "bootstrap"  : { "deps" : ['jquery'] },
+        "magnificent": { "deps" : ['jquery'] },
+        "esriLeaflet": { "deps" : ['leaflet'] },
+        "omnivore"   : { "deps" : ['leaflet'] }    },
+    paths: {
+        vignettes: './vignettes',
+        helpers: './helpers',
+        layers: './assets/layers',
+        //Cache bust the eyewitness layer if it's accessed on a different day. 
+        geoeyewitness: './assets/layers/geoeyewitness.js?bust=' + (function(){var d = new Date(); var n =d.getMonth() + d.getDay() + d.getYear(); return n})(),
+        leaflet: "//leafletjs.com/dist/leaflet",
+        esriLeaflet: 'http://cdn-geoweb.s3.amazonaws.com/esri-leaflet/1.0.0-rc.3/esri-leaflet',
+        minimap: './assets/plugins/minimap/Control.MiniMap',
+        text: './assets/plugins/text',
+        hbs: './assets/lib/require-handlebars-plugin/hbs',
+        omnivore: '//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.2.0/leaflet-omnivore.min',
+        jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
+        bootstrap: "//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min",
+        magnificent: "./assets/lib/magnificent",
+        map: "./controllers/map"
+    },
+    hbs: { // optional
+        helpers: true,            // default: true
+        i18n: false,              // default: false
+        templateExtension: 'html', // default: 'hbs'
+        partialsUrl: ''           // default: ''
+    },
+    waitSeconds: 0
+};
